b36e9adcc61d93da190e4af0e23f4837f4634012
Dingyonggang <123611684@qq.com>
2017-06-24 19:32:19 +0800

    stash后的提交

--- 冒泡排序/main.swift
+++ 冒泡排序/main.swift
@@ -9,19 +9,19 @@
 import Foundation
 
 var array:[Int] = [1,234,34,63,2,4,6,72,2,34,4,56,7,7,64,32,]
-//
-//for i in 0..<array.count {
-//    
-//    for j in 0..<array.count - (i+1) {
-//        
-//        if array[j] > array[j+1] {
-//            
-//            let temp = array[j]
-//            array[j] = array[j+1]
-//            array[j+1] = temp
-//        }
-//    }
-//}
+
+for i in 0..<array.count {
+    
+    for j in 0..<array.count - (i+1) {
+        
+        if array[j] > array[j+1] {
+            
+            let temp = array[j]
+            array[j] = array[j+1]
+            array[j+1] = temp
+        }
+    }
+}
 
 
 
